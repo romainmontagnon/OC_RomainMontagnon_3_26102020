f535d4694364f6629c4e2532a9d1d2d2799ee9e5
romainmontagnon <montagnon.romain@gmail.com>
2020-11-12 11:09:48 +0100

    debugging mobile phone checkbox issue V2

--- stylesheet/iosJSscript.js
+++ stylesheet/iosJSscript.js
@@ -1,5 +1,5 @@
 window.onload = function() {
     if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
-        document.body.addEventListener('touchstart', function() {}, false);
+        document.body.addEventListener('touchstart', function(:hover) {}, false);
     }
     };
